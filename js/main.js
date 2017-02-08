ed8cfc356ffdeb9174e3618d4bfd9690df2834a3
theodoretan <tfjtan@hotmail.com>
2017-02-07 20:20:06 -0500

    overlay!

--- js/main.js
+++ js/main.js
@@ -2,3 +2,26 @@ window.onload = () => {
 	let div = document.getElementById('is-loading');
 	div.removeAttribute('id');
 }
+
+let links = document.getElementsByClassName('links');
+let leftOverlay = document.getElementsByClassName('pre-overlay')[0];
+let rightOverlay = document.getElementsByClassName('overlay')[0];
+
+console.log(links);
+
+for (let link of links) {
+	link.onclick = (event) => {
+		event.preventDefault();
+		leftOverlay.classList.remove('fadeOutLeftOverlay');
+		rightOverlay.classList.remove('fadeOutRightOverlay');
+		leftOverlay.classList.add('fadeInLeftOverlay');
+		rightOverlay.classList.add('fadeInRightOverlay');
+	}
+}
+
+leftOverlay.onclick = (event) => {
+	leftOverlay.classList.remove('fadeInLeftOverlay');
+	rightOverlay.classList.remove('fadeInRightOverlay');
+	leftOverlay.classList.add('fadeOutLeftOverlay');
+	rightOverlay.classList.add('fadeOutRightOverlay');
+}
