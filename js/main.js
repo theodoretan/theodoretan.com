b93185607075ec4ec40a87597915a74c551b5260
theodoretan <tfjtan@hotmail.com>
2017-02-22 20:15:42 -0500

    everything except contact

--- js/main.js
+++ js/main.js
@@ -19,6 +19,12 @@ for (let link of links) {
 		leftOverlay.classList.add('fadeInLeftOverlay');
 		rightOverlay.classList.add('fadeInRightOverlay');
 		body.classList.add('no-scroll');
+
+		if (link.id == 'about') {
+			getFiles(link.id, '');
+		} else {
+			getFiles('experience', link.id);
+		}
 	}
 }
 
@@ -28,27 +34,69 @@ cancelBtn.onclick = leftOverlay.onclick = (event) => {
 	leftOverlay.classList.add('fadeOutLeftOverlay');
 	rightOverlay.classList.add('fadeOutRightOverlay');
 	body.classList.remove('no-scroll');
+
+	let innerOverlay = document.getElementsByClassName('inner-overlay')[0];
+	innerOverlay.innerHTML = '';
 }
 
 // will have to do JSON.parse() for modals
 
-// var ajax = new XMLHttpRequest();
-//
-// ajax.onreadystatechange = () => {
-// 	try {
-// 		if (ajax.readyState === XMLHttpRequest.DONE) {
-// 			if (ajax.status === 200) {
-// 				// do the stuff with ajax.responseText
-// 				// JSON.parse(ajax.responseText) probably
-// 			} else {
-// 				// will need a fallback
-// 			}
-// 		}
-// 	} catch (e) {
-// 		// could not connect
-// 	} finally {
-// 		// do something smart with the modal
-// 	}
-// }
-// ajax.open('GET', url);
-// ajax.send();
+let getFiles = (content, picker) => {
+	let url = '../assets/files/' + content + '.json';
+	var ajax = new XMLHttpRequest();
+	let c = content;
+	let p = picker;
+
+	ajax.onreadystatechange = () => {
+		try {
+			if (ajax.readyState === XMLHttpRequest.DONE) {
+				if (ajax.status === 200) {
+					let innerOverlay = document.getElementsByClassName('inner-overlay')[0];
+					innerOverlay.innerHTML = '';
+
+					let json = JSON.parse(ajax.responseText);
+
+					if (c === 'experience') {
+						json = json[p];
+						// console.log(json);
+					}
+
+					let title = json.Title;
+					let content = json.Content;
+
+					// check for subtitle
+					if (!json.Subitle) {
+						for (var key in content) {
+							for (var type in content[key]) {
+								switch (type) {
+									case 'Paragraph':
+										// create p tags
+										let p = document.createElement('p');
+										let node = document.createTextNode(content[key][type]);
+										p.appendChild(node);
+										p.className = 'overlay-p';
+										innerOverlay.appendChild(p);
+
+										break;
+									case 'Image':
+										let img = new Image();
+										img.src = '../assets/' + content[key][type];
+										img.classList.add('overlay-img');
+										innerOverlay.appendChild(img);
+										break;
+									default:
+										console.log('Unknown Type: ' + type);
+								}
+							}
+						}
+					}
+				}
+			}
+		} catch (e) {
+			console.log('could not connect');
+			console.log(e);
+		}
+	}
+	ajax.open('GET', url);
+	ajax.send();
+}
