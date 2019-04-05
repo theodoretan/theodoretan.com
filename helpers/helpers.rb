19bab9d5ed1aabaaba818473a6ba58d6c3f841d1
Theodore Tan <theodore.tan@shopify.com>
2019-04-04 21:18:14 -0400

    Remove unused helper file

--- helpers/helpers.rb
+++
@@ -1,7 +0,0 @@
-helpers do
-  def find_template(views, name, engine, &block)
-    _, folder = views.detect { |k,v| engine == Tilt[k] }
-    folder ||= views[:default]
-    super(folder, name, engine, &block)
-  end
-end
